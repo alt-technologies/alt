90757416ffc25a0dadfedb1f73037016873c42aa
alt-technologies <zahid.rsldubai@gmail.com>
2023-04-20 09:16:48 +0400

    Contact changes 1

--- .next/static/webpack/webpack.63241b6b1980932f.hot-update.js
+++ .next/static/webpack/webpack.63241b6b1980932f.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "dcf8aa260d1cab3c"; }
+/******/ 	__webpack_require__.h = function() { return "b32b7bc4d290e55d"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
